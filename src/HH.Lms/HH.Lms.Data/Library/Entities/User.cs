c5ecd2abe2bfde553fbd49d7cb7e940709d03a37
hamadhassan3 <hamadhassan3@gmail.com>
2023-11-01 16:57:44 +0500

    Data layer initial setup

---
+++ src/HH.Lms/HH.Lms.Data/Library/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using HH.Lms.Data.Common;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace HH.Lms.Data.Library.Entities
+{
+    public class User : IdentityUser, IBaseEntity
+    {
+        [PersonalData] public string FirstName { get; set; }
+
+        [PersonalData] public string LastName { get; set; }
+
+        public string Role { get; set; }
+
+        [Display(Name = "Name")] public string FullName => $"{FirstName} {LastName}";
+
+        // Linking to Book (One to many)
+        public ICollection<Book> Books { get; set; }
+    }
+}
---
+++ src/HH.Lms/HH.Lms.Data/Repositories/EntityRepositories/BookRepository.cs
@@ -0,0 +1,12 @@
+﻿using HH.Lms.Data.Library;
+using HH.Lms.Data.Library.Entities;
+
+namespace HH.Lms.Data.Repository.EntityRepository
+{
+    public class BookRepository: GenericRepository<Book>
+    {
+        public BookRepository(LibraryDBContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
