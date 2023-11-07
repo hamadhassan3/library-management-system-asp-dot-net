36e55f5d969320c90c0dc855606e45da2bce822d
hamadhassan3 <hamadhassan3@gmail.com>
2023-11-07 17:32:48 +0500

    Added interceptor

--- src/HH.Lms/HH.Lms.Api/Startup.cs
+++ src/HH.Lms/HH.Lms.Api/Startup.cs
@@ -1,4 +1,10 @@
+<<<<<<< Updated upstream
 ﻿using Microsoft.AspNetCore.Builder;
+=======
+﻿using HH.Lms.Data.Interceptors;
+using HH.Lms.Data.Library;
+using Microsoft.AspNetCore.Builder;
+>>>>>>> Stashed changes
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +24,17 @@ namespace HH.Lms.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+<<<<<<< Updated upstream
+=======
+
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                services.AddDbContext<LibraryDBContext>(options => {
+                    options.UseMySQL(connectionString);
+                    options.AddInterceptors(new DatabaseInterceptor());
+                });
+            }
+>>>>>>> Stashed changes
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
