856aa16518ec5b21241e97ed0fc1c4221afcb554
Hammad Hassan <60880656+hamadhassan3@users.noreply.github.com>
2023-11-07 17:40:07 +0500

    Added interceptor (#5)

---
+++ src/HH.Lms/HH.Lms.Data/Interceptors/DatabaseInterceptor.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Common;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace HH.Lms.Data.Interceptors;
+
+public class DatabaseInterceptor : DbCommandInterceptor
+{
+    public override InterceptionResult<DbDataReader> ReaderExecuting(
+        DbCommand command,
+        CommandEventData eventData,
+        InterceptionResult<DbDataReader> result)
+    {
+        LogQuery(command.CommandText);
+        return base.ReaderExecuting(command, eventData, result);
+    }
+
+    public override InterceptionResult<object> ScalarExecuting(
+        DbCommand command,
+        CommandEventData eventData,
+        InterceptionResult<object> result)
+    {
+        LogQuery(command.CommandText);
+        return base.ScalarExecuting(command, eventData, result);
+    }
+
+    public override InterceptionResult<int> NonQueryExecuting(
+        DbCommand command,
+        CommandEventData eventData,
+        InterceptionResult<int> result)
+    {
+        LogQuery(command.CommandText);
+        return base.NonQueryExecuting(command, eventData, result);
+    }
+
+    private void LogQuery(string query)
+    {
+        // You can log the query here using your preferred logging mechanism
+        Console.WriteLine($"Executing query: {query}");
+        Debug.WriteLine($"Executing query: {query}");
+    }
+}
