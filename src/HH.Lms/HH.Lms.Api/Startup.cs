7c98b1c592e5655b458d8b294b11fbf0a336da0e
hamadhassan3 <hamadhassan3@gmail.com>
2023-11-08 12:30:21 +0500

    Fixed directory location for api project.

--- src/HH.Lms/HH.Lms.Api/Startup.cs
+++ src/HH.Lms/HH.Lms.Api/Startup.cs
@@ -1,55 +1,50 @@
 ﻿using HH.Lms.Data.Interceptors;
 using HH.Lms.Data.Library;
-using Microsoft.AspNetCore.Builder;
-﻿using HH.Lms.Data.Library;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace HH.Lms.Api
+namespace HH.Lms.Api;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var connectionString = Configuration.GetConnectionString("DefaultConnection");
+        services.AddControllers();
+        if (!string.IsNullOrEmpty(connectionString))
         {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            var connectionString = Configuration.GetConnectionString("DefaultConnection");
-            services.AddControllers();
-
-            if (!string.IsNullOrEmpty(connectionString))
-            {
-                services.AddDbContext<LibraryDBContext>(options => {
-                    options.UseMySQL(connectionString);
-                    options.AddInterceptors(new DatabaseInterceptor());
-                });
-            }
-
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            services.AddDbContext<LibraryDBContext>(options => {
+                options.UseMySQL(connectionString);
+                options.AddInterceptors(new DatabaseInterceptor());
             });
         }
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Your API", Version = "v1" });
+        });
+
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
     }
 }
