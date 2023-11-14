3716e7390003ac526a1d2cffde80c0c2b664d20a
hamadhassan3 <hamadhassan3@gmail.com>
2023-11-14 17:05:25 +0500

    JWT setup complete.

--- src/HH.Lms/HH.Lms.Service/Library/Dto/BookDto.cs
+++ src/HH.Lms/HH.Lms.Service/Library/Dto/BookDto.cs
@@ -1,26 +1,19 @@
 ﻿using HH.Lms.Service.Base;
-using Org.BouncyCastle.Bcpg;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HH.Lms.Service.Library.Dto
+namespace HH.Lms.Service.Library.Dto;
+
+public class BookDto : IDto
 {
-    public class BookDto : IDto
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        public string Title { get; set; }
+    public string Title { get; set; }
 
-        public string Description { get; set; }
+    public string Description { get; set; }
 
-        public string Isbn { get; set; }
+    public string Isbn { get; set; }
 
-        public string Author { get; set; }
+    public string Author { get; set; }
 
-        public int UserId { get; set; }
+    public int UserId { get; set; }
 
-    }
 }
